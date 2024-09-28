fd92ed8891782129d9f596cdbdcf30872f2c2709
NayMcE <naomi.mcewan2@admiralgroup.co.uk>
2024-09-28 15:53:41 +0100

    more codewars

--- codewars.py
+++ codewars.py
@@ -5,8 +5,8 @@
 #     else:
 #         print("hello guest")
 
-# 
-# 
+
+# function to calculate BMI
 # def bmi(weight, height):
 #     bmi = weight/height ** 2
 #     if bmi >= 0:
@@ -24,10 +24,41 @@
 
 # print(bmi(80, 1.80))
 
-def no_space(x):
-    translation_table = str.maketrans('','',' ')
-    string_without_spaces = x.translate(translation_table)
-    return string_without_spaces
+# # removing spaces from strings
+# def no_space(x):
+#     translation_table = str.maketrans('','',' ')
+#     string_without_spaces = x.translate(translation_table)
+#     return string_without_spaces
 
-print(no_space('H e l l o'))
+# print(no_space('H e l l o'))
 
+#function that greets the world
+# def hello_world():
+#     print('Hello World')
+
+# print(hello_world())    
+
+#function that returns a string from a boolean
+# def bool_to_word(boolean):
+#     if boolean == True:
+#         return 'Yes'
+#     elif boolean == False:
+#         return 'No'
+    
+# print(bool_to_word(True))
+
+#method to use additive inverse on a number given
+# def opposite(number):
+#     return number * -1
+
+# print(opposite(3))
+
+def area_or_perimeter(l, w):
+    area = l * w
+    perimeter = 2 * (l+w)
+    if l == w:
+        return area
+    else:
+        return perimeter
+    
+print(area_or_perimeter(3, 3))
