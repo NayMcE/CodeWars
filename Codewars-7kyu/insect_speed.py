ce3a7a8b4781d2dd1c39b77930285b149b1d408a
NayMcE <naomi.mcewan2@admiralgroup.co.uk>
2024-09-30 16:40:19 +0100

    added more examples and moved to 7kyu

---
+++ Codewars-7kyu/insect_speed.py
@@ -0,0 +1,6 @@
+def cockroach_speed(s):
+    cm_persecond = s * 27.777778
+    intspeed = int(cm_persecond)
+    return intspeed
+
+print(cockroach_speed(1.08))
--- Codewars-8kyu/array_count_sheep.py
+++ Codewars-8kyu/array_count_sheep.py
@@ -1,7 +1,14 @@
-def count_sheeps(sheep):
-    return sum([1 for s in sheep if s])
+# def count_sheeps(sheep):
+#     return sum([1 for s in sheep if s])
 
-print(count_sheeps([True, True, False, True]))
+# print(count_sheeps([True, True, False, True]))
 
 
-        
+def can_pay(price, cash_given):
+    if cash_given >= price:
+        return True
+    else:
+        return False
+
+
+print(can_pay(100, 250))
